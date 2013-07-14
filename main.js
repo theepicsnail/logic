f15fce52b342d19dd93258c05f626a5673c9f73c
Steven <theepicsnail@gmail.com>
2013-07-14 02:24:53 -0400

    Cleaned up main.js a bit. Added 'loading' text.

--- main.js
+++ main.js
@@ -1,91 +1,118 @@
+var stage;
+var layer;
+var loadingAnim;
+var loadingText;
+var gateImage;
 window.onload = function () {
-      console.log("A")
-      var stage = new Kinetic.Stage({
-        container: 'container',
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
 
-      var layer = new Kinetic.Layer();
+  stage = new Kinetic.Stage({
+    container: 'container',
+    width: window.innerWidth,
+    height:window.innerHeight
+  });
 
-      var rect = new Kinetic.Rect({
-        x: 239,
-        y: 75,
-        width: 100,
-        height: 50,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 4
-      });
+  layer = new Kinetic.Layer();
+  stage.add(layer);
+  window.onresize = function(e) {
+    stage.setSize(window.innerWidth, window.innerHeight)
+  }
 
-      // add the shape to the layer
+  loadingText = new Kinetic.Text({
+    x:0,
+    y:20,
+    text:"Loading", 
+    fontSize: 30,
+    fontFamily: 'Calibri',
+    fill: 'black'
+  });
 
+  loadingAnim = new Kinetic.Animation(function(frame) {
+    loadingText.setOpacity(.7 + .3 * Math.sin(frame.time * .005))
+    loadingText.setPosition(stage.getWidth()/2 - 60, stage.getHeight()/2)
+  }, layer);
 
-      var crops = {}
-      var y = 0
-      crops["NOT"] ={x:0, y:y, width:162, height:64}; y += 64; 
-      crops["AND"] ={x:0, y:y, width:162, height:64}; y += 64; 
-      crops["NAND"]={x:0, y:y, width:162, height:64}; y += 64; 
-      crops["OR"]  ={x:0, y:y, width:162, height:64}; y += 64; 
-      crops["NOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
-      crops["XOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
-      crops["XNOR"]={x:0, y:y, width:162, height:64}; y += 64; 
-      var gateImages = {};
+  loadingAnim.start();
+  layer.add(loadingText);
 
-      var imageObj = new Image();
-      imageObj.onload = function() { 
+  gateImage = new Image();
+  gateImage.onload = loadFinished
+  gateImage.src = "images/gates.png"
+}
 
-        var b = new Kinetic.Bezier({
-          startPoint:{x:0,y:0},
-          controlPoint1:{x:50,y:0},
-          controlPoint2:{x:50,y:100},
-          endPoint:{x:100,y:100}
-        })
-        layer.add(b)
+function loadFinished() {
+  loadingAnim.stop()
+  loadingText.remove()
 
+  var crops = {}
+  var y = 0
+  crops["NOT"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  crops["AND"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  crops["NAND"]={x:0, y:y, width:162, height:64}; y += 64; 
+  crops["OR"]  ={x:0, y:y, width:162, height:64}; y += 64; 
+  crops["NOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  crops["XOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  crops["XNOR"]={x:0, y:y, width:162, height:64}; y += 64; 
+  var gateImages = {};
 
-
-        var gate = new Kinetic.Image({crop:crops["NOT"], image:imageObj, width: 162, height:64, draggable:true, scale:.5});
-        layer.add(gate)
-        gate.setPosition(100,100)
-        
-        var gate2 = new Kinetic.Image({crop:crops["OR"], image:imageObj, width: 162, height:64, draggable:true, scale:.5});
-        layer.add(gate2)
-        gate2.setPosition(50,50)
-
-
-        gate.on('dragmove', function(m) {
-          target = m.targetNode.attrs
-          b.startPoint.x = target.x + target.width * target.scaleX
-          b.startPoint.y = target.y + target.height/2* target.scaleY
-          b.controlPoint1.x = b.startPoint.x + 100
-          b.controlPoint1.y = b.startPoint.y
-        })
-
-        gate2.on('dragmove', function(m) {
-          target = m.targetNode.attrs
-          b.endPoint.x = target.x
-          b.endPoint.y = target.y + (target.height/2 - 15)* target.scaleY
-          b.controlPoint2.x = b.endPoint.x - 100
-          b.controlPoint2.y = b.endPoint.y 
-        })
+  var b = new Kinetic.Bezier({
+    startPoint:{x:0,y:0},
+    controlPoint1:{x:50,y:0},
+    controlPoint2:{x:50,y:100},
+    endPoint:{x:100,y:100}
+  })
+  layer.add(b)
 
 
 
-        /*
-        var i = new Kinetic.Image({image:imageObj, x:0, y:0})
-        layer.add(i)
-        /**/
-        layer.add(rect);
+  var gate = new Kinetic.Image({crop:crops["NOT"], image:gateImage, width: 162, height:64, draggable:true, scale:.5});
+  layer.add(gate)
+  gate.setPosition(100,100)
 
-      // add the layer to the stage
-      stage.add(layer);
-      };
-      imageObj.src = "images/gates.png"
+  var gate2 = new Kinetic.Image({crop:crops["OR"], image:gateImage, width: 162, height:64, draggable:true, scale:.5});
+  layer.add(gate2)
+  gate2.setPosition(50,50)
 
-      window.onresize = function(e) {
-        stage.setSize(e.target.outerWidth, e.target.outerHeight)
-      }
-      window.l = layer
+
+  gate.on('dragmove', function(m) {
+    target = m.targetNode.attrs
+    b.startPoint.x = target.x + target.width * target.scaleX
+    b.startPoint.y = target.y + target.height/2* target.scaleY
+    b.controlPoint1.x = b.startPoint.x + 100
+    b.controlPoint1.y = b.startPoint.y
+  })
+
+  gate2.on('dragmove', function(m) {
+    target = m.targetNode.attrs
+    b.endPoint.x = target.x
+    b.endPoint.y = target.y + (target.height/2 - 15)* target.scaleY
+    b.controlPoint2.x = b.endPoint.x - 100
+    b.controlPoint2.y = b.endPoint.y 
+  })
+
+
+  var rect;
+  rect = new Kinetic.Rect({
+    x: 239,
+    y: 75,
+    width: 100,
+    height: 50,
+    fill: 'green',
+    stroke: 'black',
+    strokeWidth: 4
+  });
+
+  layer.add(rect);
+  layer.draw()
+
+
+  /*
+  var i = new Kinetic.Image({image:imageObj, x:0, y:0})
+  layer.add(i)
+  /**/
+  layer.add(rect);
+
+  // add the layer to the stage
+
+  layer.draw()
 }
 
