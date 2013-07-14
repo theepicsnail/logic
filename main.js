370cfc8a20f02db5ff0f44db073bf39592a50c9f
Steven <theepicsnail@gmail.com>
2013-07-13 23:59:55 -0400

    moved js to its own file. Fixed default resolution.

---
+++ main.js
@@ -0,0 +1,91 @@
+window.onload = function () {
+      console.log("A")
+      var stage = new Kinetic.Stage({
+        container: 'container',
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+
+      var layer = new Kinetic.Layer();
+
+      var rect = new Kinetic.Rect({
+        x: 239,
+        y: 75,
+        width: 100,
+        height: 50,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 4
+      });
+
+      // add the shape to the layer
+
+
+      var crops = {}
+      var y = 0
+      crops["NOT"] ={x:0, y:y, width:162, height:64}; y += 64; 
+      crops["AND"] ={x:0, y:y, width:162, height:64}; y += 64; 
+      crops["NAND"]={x:0, y:y, width:162, height:64}; y += 64; 
+      crops["OR"]  ={x:0, y:y, width:162, height:64}; y += 64; 
+      crops["NOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
+      crops["XOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
+      crops["XNOR"]={x:0, y:y, width:162, height:64}; y += 64; 
+      var gateImages = {};
+
+      var imageObj = new Image();
+      imageObj.onload = function() { 
+
+        var b = new Kinetic.Bezier({
+          startPoint:{x:0,y:0},
+          controlPoint1:{x:50,y:0},
+          controlPoint2:{x:50,y:100},
+          endPoint:{x:100,y:100}
+        })
+        layer.add(b)
+
+
+
+        var gate = new Kinetic.Image({crop:crops["NOT"], image:imageObj, width: 162, height:64, draggable:true, scale:.5});
+        layer.add(gate)
+        gate.setPosition(100,100)
+        
+        var gate2 = new Kinetic.Image({crop:crops["OR"], image:imageObj, width: 162, height:64, draggable:true, scale:.5});
+        layer.add(gate2)
+        gate2.setPosition(50,50)
+
+
+        gate.on('dragmove', function(m) {
+          target = m.targetNode.attrs
+          b.startPoint.x = target.x + target.width * target.scaleX
+          b.startPoint.y = target.y + target.height/2* target.scaleY
+          b.controlPoint1.x = b.startPoint.x + 100
+          b.controlPoint1.y = b.startPoint.y
+        })
+
+        gate2.on('dragmove', function(m) {
+          target = m.targetNode.attrs
+          b.endPoint.x = target.x
+          b.endPoint.y = target.y + (target.height/2 - 15)* target.scaleY
+          b.controlPoint2.x = b.endPoint.x - 100
+          b.controlPoint2.y = b.endPoint.y 
+        })
+
+
+
+        /*
+        var i = new Kinetic.Image({image:imageObj, x:0, y:0})
+        layer.add(i)
+        /**/
+        layer.add(rect);
+
+      // add the layer to the stage
+      stage.add(layer);
+      };
+      imageObj.src = "images/gates.png"
+
+      window.onresize = function(e) {
+        stage.setSize(e.target.outerWidth, e.target.outerHeight)
+      }
+      window.l = layer
+}
+
