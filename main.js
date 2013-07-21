cd625e826eebe077ea891d4bfe93d376e499c015
Steven <theepicsnail@gmail.com>
2013-07-21 05:44:37 -0400

    Refactored scene building
    Also reworked tookbox to workspace item creation
    Added in some circle objects for input/output markers

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-var stage;
-var layer;
-var loadingAnim;
-var loadingText;
-var gateImage;
+var stage, gateImages={};
 window.onload = function () {
 
   stage = new Kinetic.Stage({
@@ -11,6 +7,7 @@ window.onload = function () {
     height:window.innerHeight
   });
 
+  var layer;
   layer = new Kinetic.Layer();
   stage.add(layer);
   window.onresize = function(e) {
@@ -34,18 +31,41 @@ window.onload = function () {
   loadingAnim.start();
   layer.add(loadingText);
 
-  gateImage = new Image();
-  gateImage.onload = loadFinished
+  var gateImage = new Image();
+  gateImage.onload = function () {
+
+    parseImages(gateImage)
+    buildToolbox()
+
+    loadingAnim.stop()
+    layer.remove()
+  }
   gateImage.src = "images/gates.png"
 }
 
-function loadFinished() {
-  loadingAnim.stop()
-  loadingText.remove()
+function parseImages(gateImage) {
+  var configs = {}
+  var y = 0
+  configs["NOT"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  configs["AND"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  configs["NAND"]={x:0, y:y, width:162, height:64}; y += 64; 
+  configs["OR"]  ={x:0, y:y, width:162, height:64}; y += 64; 
+  configs["NOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  configs["XOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
+  configs["XNOR"]={x:0, y:y, width:162, height:64}; y += 64; 
+  gateImages = {}
+  for (var gate in configs) {
+    gateImages[gate] = new Kinetic.Image({
+      crop:configs[gate],
+      width: 162, height:64, scale:.5,
+      image:gateImage
+    })
+  }
+}
 
-  var toolbox = new Kinetic.Layer()
-  stage.add(toolbox)
+function buildToolbox() {
 
+  toolbox = new Kinetic.Layer()
   var rect;
   rect = new Kinetic.Rect({
     x: 2,
@@ -56,24 +76,97 @@ function loadFinished() {
     stroke: 'black',
     strokeWidth: 4
   });
-  toolbox.add(rect);
-  
-  var crops = {}
-  var y = 0
-  crops["NOT"] ={x:0, y:y, width:162, height:64}; y += 64; 
-  crops["AND"] ={x:0, y:y, width:162, height:64}; y += 64; 
-  crops["NAND"]={x:0, y:y, width:162, height:64}; y += 64; 
-  crops["OR"]  ={x:0, y:y, width:162, height:64}; y += 64; 
-  crops["NOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
-  crops["XOR"] ={x:0, y:y, width:162, height:64}; y += 64; 
-  crops["XNOR"]={x:0, y:y, width:162, height:64}; y += 64; 
-  var gateImages = {};
-  var gates =  ["AND", "NAND", "OR", "NOR", "XOR", "XNOR", "NOT"]
+  toolbox.add(rect)
 
+  var orderedGates = ["AND",  "NAND", 
+                      "OR",   "NOR",
+                      "XOR",  "XNOR",
+                      "NOT"]
+  var leftCol = true;
+  var row = 0;
+  for (var id in orderedGates) {
+    var gate = orderedGates[id]
+
+    var y = 6 + row * 70
+    var x = 9
+    if (!leftCol) {
+      x += 90
+      row += 1
+    }
+    leftCol = !leftCol;
+   
+    (function (x,y, gate) { 
+    var img = gateImages[gate]
+    img.setPosition(x,y)
+    toolbox.add(img)
+    img.on('mousedown', function() {
+      g = newGate(gate)
+      g.setPosition(x,y)
+      workspace.add(g)
+      console.log(g)
+      stage.draw()
+
+      g.startDrag()
+    })
+    })(x,y,gate)
+  }
+
+  stage.add(toolbox)
+
+  var workspace = new Kinetic.Layer()
+  stage.add(workspace)
+}
+
+function newGate(gateType) {
+  var group = new Kinetic.Group({
+      draggable:true})
+  var src = gateImages[gateType]
+  //return src
+  var gate = new Kinetic.Image({
+      x:src.getX(),
+      y:src.getY(),
+      width:src.getWidth(),
+      height:src.getHeight(),
+      crop:src.getCrop(),
+      image:src.getImage(),
+      scale:src.getScale(),
+  })
+  gate.setPosition(0,0) 
+  gate.on('dragend', function(e){
+    if (e.x < 200)
+    {
+      var l = gate.getLayer()
+      gate.remove()
+      l.draw()
+    }
+  })
+  group.add(gate)
+
+  var out = new Kinetic.Circle({
+    x:76, y:16, radius:5, fill:'black', strokeWidth:2, stroke:'black'
+  })
+  group.add(out)
+
+  var in1 = new Kinetic.Circle({
+    x:4, y:10, radius:5, fill:'blue', strokeWidth:2, stroke:'black'
+  })
+  group.add(in1)
+  if (gateType == "NOT") {
+    in1.setY(17)
+  } else {
+    var in2 = new Kinetic.Circle({
+      x:4, y:25, radius:5, fill:'red', strokeWidth:2, stroke:'black'
+    })
+    group.add(in2)
+  }
+  return group
+}
+
+/*  
   for(var idx in gates){
     newGate = function(idx) {
       console.log("Created gate:" + gates[idx])
-      var gate = new Kinetic.Image({crop:crops[gates[idx]], image:gateImage, 
+      var gate = new Kinetic.Image({crop:configs[gates[idx]], 
           width:162, height:64, draggable:true, scale:.5});
       gate.setPosition(idx%2*90+9, Math.floor(idx/2)*70+6)
       toolbox.add(gate)
@@ -86,20 +179,12 @@ function loadFinished() {
         gateInToolbox = false
         gate.moveToTop()
       })
-      gate.on('dragend', function(e){
-        if (e.x < 200)
-        {
-          var l = gate.getLayer()
-          gate.remove()
-          l.draw()
-        }
-      })
-    }
+     }
     newGate(idx)
   }
 
-
-
+*/
+function unused() {
   var b = new Kinetic.Bezier({
     startPoint:{x:0,y:0},
     controlPoint1:{x:50,y:0},
@@ -110,12 +195,12 @@ function loadFinished() {
 
 
 
-  var gate = new Kinetic.Image({crop:crops["NOT"], image:gateImage, 
+  var gate = new Kinetic.Image({crop:configs["NOT"], 
         width: 162, height:64, draggable:true, scale:.5});
   layer.add(gate)
   gate.setPosition(100,100)
 
-  var gate2 = new Kinetic.Image({crop:crops["OR"], image:gateImage, 
+  var gate2 = new Kinetic.Image({crop:configs["OR"], 
         width: 162, height:64, draggable:true, scale:.5});
   layer.add(gate2)
   gate2.setPosition(50,50)
